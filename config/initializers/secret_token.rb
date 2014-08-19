22dbbb3a6731dedbdc38f1cb3e5457e47cacc874
Abraham R Burnett <abeburnett@gmail.com>
2014-08-19 00:58:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogSprintThree::Application.config.secret_key_base = 'f088beb897b37d59cf64b265594097ec6a9e0795a2e558d9ae006acc3181890232b93c5b749f09e37e05c6befe4a820af8ffe3832639618ef5708740d735e053'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogSprintThree::Application.config.session_store :cookie_store, key: '_blog_sprint_three_session'
